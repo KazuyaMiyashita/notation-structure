d83a4b8e9f3e52411c7ecbb5acf4e9ee79f91bd6
KazuyaMiayshita <k_miyashita@itandi.co.jp>
2019-09-05 02:45:45 +0900

    transpose

--- src/main/scala/core/NoteSequence.scala
+++ src/main/scala/core/NoteSequence.scala
@@ -4,6 +4,8 @@ case class NoteSequence(notes: Seq[Note]) extends Seq[Note] {
   override def apply(i: Int): Note = notes.apply(i)
   override def iterator: Iterator[Note] = notes.iterator
   override def length: Int = notes.length
+
+  def transpose(that: Note) = NoteSequence(map(_ + that))
 }
 
 object NoteSequence {
