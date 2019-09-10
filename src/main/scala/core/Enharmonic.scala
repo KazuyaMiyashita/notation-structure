0580836adb7f5b6834195510ce4a754a5ef805a3
KazuyaMiayshita <miy1924@gmail.com>
2019-09-10 17:48:56 +0900

    wip

--- src/main/scala/core/Enharmonic.scala
+++ src/main/scala/core/Enharmonic.scala
@@ -3,8 +3,12 @@ package core
 case class Enharmonic(midiNoteNumber: MidiNoteNumber) {
 
   def pitchs: Set[Pitch] = {
-    val fifths = -15 to 19
-    ???
+    val ps = for {
+      fifth <- -15 to 19
+      n = (midiNoteNumber.value - 60) - (7 * fifth)
+      oct = n / 12 if n % 12 == 0
+    } yield Pitch(oct, FifthName(fifth))
+    ps.toSet
   }
 
 }
@@ -13,6 +17,10 @@ object Enharmonic {
 
   def fromMidiNoteNumber(midiNoteNumber: MidiNoteNumber): Enharmonic = {
     new Enharmonic(midiNoteNumber)
+  }
+
+  def combinate(enharmonics: Seq[Enharmonic]): Seq[Set[Pitch]] = {
+    ???
   }
 
 }
