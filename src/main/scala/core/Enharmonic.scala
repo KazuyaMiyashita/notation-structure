c00d13294858ab03a5f61f8c16b7479f295b36b6
KazuyaMiayshita <miy1924@gmail.com>
2019-09-11 10:51:44 +0900

    wip 異名同音の組み合わせ、むやみに全パターン作って重々増一度とか含めるとコード判定の誤判定出すので削りたい

--- src/main/scala/core/Enharmonic.scala
+++ src/main/scala/core/Enharmonic.scala
@@ -2,14 +2,15 @@ package core
 
 case class Enharmonic(midiNoteNumber: MidiNoteNumber) {
 
-  def pitchs: Set[Pitch] = {
+  def pitchs: List[Pitch] = {
     val ps = for {
       fifth <- -15 to 19
       n = (midiNoteNumber.value - 60) - (7 * fifth) if n % 12 == 0
       oct = n / 12
     } yield Pitch(oct, FifthName(fifth))
-    ps.toSet
+    ps.toList
   }
+  def pitchSet: Set[Pitch] = pitchs.toSet
 
 }
 
@@ -19,8 +20,17 @@ object Enharmonic {
     new Enharmonic(midiNoteNumber)
   }
 
-  def combinate(enharmonics: Seq[Enharmonic]): Seq[Set[Pitch]] = {
-    ???
+  def combinate(enharmonics: Set[Enharmonic]): Set[Set[Pitch]] = {
+    def proc(remaining: List[Enharmonic], acc: List[List[Pitch]])
+      :(List[Enharmonic], List[List[Pitch]]) = {
+
+      remaining match {
+        case Nil => (Nil, acc)
+        case enh :: ts => proc(ts, enh.pitchs.map(ps => acc.map(ac => (ps :: Nil) ::: ac)).flatten)
+      }
+    }
+
+    proc(enharmonics.toList, Nil :: Nil)._2.map(_.toSet).toSet
   }
 
 }
--- src/test/scala/core/EnharmonicSpec.scala
+++ src/test/scala/core/EnharmonicSpec.scala
@@ -5,20 +5,128 @@ import PitchName._
 
 class EnharmonicSpec extends WordSpec with Matchers {
 
-  def midinameof(midiNum: Int) = Enharmonic(MidiNoteNumber(midiNum)).pitchs
+  def enharmonic(midiNum: Int): Enharmonic = Enharmonic(MidiNoteNumber(midiNum))
+  def conbinations(midiNums: Int*): Set[Set[Pitch]] = {
+    Enharmonic.combinate(midiNums.toSet.map((n: Int) => Enharmonic(MidiNoteNumber(n))))
+  }
 
   "def pitchs" should {
 
     "60 to (Bs3, C4, Dbb4)" in {
-      midinameof(60) shouldEqual Set(Bs3, C4, Dbb4)
+      enharmonic(60).pitchSet shouldEqual Set(Bs3, C4, Dbb4)
     }
 
     "62 to (Css4, D4, Ebb4)" in {
-      midinameof(62) shouldEqual Set(Css4, D4, Ebb4)
+      enharmonic(62).pitchSet shouldEqual Set(Css4, D4, Ebb4)
     }
 
     "68 to (Gs4, Ab4)" in {
-      midinameof(68) shouldEqual Set(Gs4, Ab4)
+      enharmonic(68).pitchSet shouldEqual Set(Gs4, Ab4)
+    }
+
+  }
+
+  "def combinate" should {
+
+    "conbinations(60, 63)" in {
+      conbinations(60, 63) shouldEqual Set(
+        Set(Bs3, Ds4),
+        Set(Bs3, Eb4),
+        Set(Bs3, Fbb4), // ? 重減五度
+        Set(C4, Ds4),
+        Set(C4, Eb4),
+        Set(C4, Fbb4), // ? 重々減三度
+        Set(Dbb4, Ds4), // ? 重々増一度
+        Set(Dbb4, Eb4),
+        Set(Dbb4, Fbb4)
+      )
+    }
+
+    "conbinations(60, 63, 66, 69)" in {
+
+      conbinations(60, 63, 66, 69) shouldEqual Set(
+        Set(Bbb4, Gb4, Eb4, Dbb4),
+        Set(A4, Fs4, Fbb4, Dbb4),
+        Set(Gss4, Fs4, Fbb4, C4),
+        Set(A4, Ess4, Eb4, C4),
+        Set(Gss4, Ess4, Eb4, Bs3),
+        Set(Bbb4, Ess4, Fbb4, C4),
+        Set(Gss4, Gb4, Fbb4, Bs3),
+        Set(A4, Fs4, Ds4, C4),
+        Set(Gss4, Ess4, Ds4, Bs3),
+        Set(A4, Gb4, Ds4, C4),
+        Set(Bbb4, Gb4, Fbb4, Dbb4),
+        Set(A4, Fs4, Fbb4, C4),
+        Set(Gss4, Gb4, Ds4, C4),
+        Set(A4, Gb4, Eb4, Dbb4),
+        Set(Bbb4, Fs4, Fbb4, Bs3),
+        Set(Gss4, Ess4, Eb4, C4),
+        Set(A4, Ess4, Ds4, Bs3),
+        Set(A4, Fs4, Fbb4, Bs3),
+        Set(A4, Gb4, Eb4, C4),
+        Set(A4, Ess4, Eb4, Dbb4),
+        Set(A4, Fs4, Ds4, Bs3),
+        Set(A4, Ess4, Fbb4, Dbb4),
+        Set(Gss4, Gb4, Fbb4, Dbb4),
+        Set(Bbb4, Gb4, Ds4, Bs3),
+        Set(A4, Fs4, Ds4, Dbb4),
+        Set(Gss4, Gb4, Fbb4, C4),
+        Set(Gss4, Gb4, Ds4, Bs3),
+        Set(Gss4, Ess4, Fbb4, C4),
+        Set(A4, Gb4, Ds4, Dbb4),
+        Set(Bbb4, Fs4, Ds4, C4),
+        Set(Gss4, Ess4, Ds4, C4),
+        Set(Bbb4, Gb4, Ds4, C4),
+        Set(A4, Gb4, Ds4, Bs3),
+        Set(A4, Ess4, Fbb4, Bs3),
+        Set(Gss4, Gb4, Eb4, C4),
+        Set(Bbb4, Ess4, Fbb4, Bs3),
+        Set(Bbb4, Ess4, Eb4, Bs3),
+        Set(Gss4, Ess4, Fbb4, Dbb4),
+        Set(Gss4, Gb4, Ds4, Dbb4),
+        Set(Bbb4, Ess4, Fbb4, Dbb4),
+        Set(A4, Fs4, Eb4, C4),
+        Set(Bbb4, Fs4, Eb4, Dbb4),
+        Set(Bbb4, Ess4, Eb4, Dbb4),
+        Set(A4, Ess4, Ds4, Dbb4),
+        Set(Gss4, Fs4, Ds4, Bs3),
+        Set(Gss4, Ess4, Eb4, Dbb4),
+        Set(Bbb4, Fs4, Eb4, Bs3),
+        Set(Gss4, Fs4, Fbb4, Dbb4),
+        Set(Bbb4, Ess4, Ds4, Dbb4),
+        Set(Bbb4, Ess4, Ds4, Bs3),
+        Set(A4, Ess4, Eb4, Bs3),
+        Set(Bbb4, Ess4, Ds4, C4),
+        Set(Bbb4, Gb4, Eb4, Bs3),
+        Set(Bbb4, Fs4, Ds4, Dbb4),
+        Set(A4, Ess4, Ds4, C4),
+        Set(A4, Gb4, Fbb4, C4),
+        Set(A4, Ess4, Fbb4, C4),
+        Set(A4, Gb4, Fbb4, Bs3),
+        Set(Gss4, Gb4, Eb4, Bs3),
+        Set(Gss4, Fs4, Eb4, Bs3),
+        Set(Gss4, Ess4, Fbb4, Bs3),
+        Set(A4, Fs4, Eb4, Dbb4),
+        Set(Bbb4, Gb4, Ds4, Dbb4),
+        Set(Bbb4, Fs4, Ds4, Bs3),
+        Set(Bbb4, Fs4, Fbb4, C4),
+        Set(Gss4, Fs4, Fbb4, Bs3),
+        Set(Bbb4, Fs4, Eb4, C4),
+        Set(Bbb4, Gb4, Fbb4, Bs3),
+        Set(Gss4, Fs4, Eb4, Dbb4),
+        Set(A4, Fs4, Eb4, Bs3),
+        Set(Bbb4, Ess4, Eb4, C4),
+        Set(Bbb4, Fs4, Fbb4, Dbb4),
+        Set(Gss4, Ess4, Ds4, Dbb4),
+        Set(Bbb4, Gb4, Eb4, C4),
+        Set(Gss4, Fs4, Ds4, C4),
+        Set(A4, Gb4, Fbb4, Dbb4),
+        Set(Gss4, Fs4, Eb4, C4),
+        Set(Gss4, Gb4, Eb4, Dbb4),
+        Set(Bbb4, Gb4, Fbb4, C4),
+        Set(Gss4, Fs4, Ds4, Dbb4),
+        Set(A4, Gb4, Eb4, Bs3)
+      )
     }
 
   }
